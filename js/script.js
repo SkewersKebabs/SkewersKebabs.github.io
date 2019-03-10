d565b7139d5a85913c834010f3ad9646ab831965
Jaren <jarenrcampbell@gmail.com>
2019-03-10 16:54:25 -0600

    New banner! Navigation options are split, logo is in middle, and scroll transition applies to logo.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ $(document).on("scroll", function(){
     if
   ($(document).scrollTop() > 50){
       $("header").addClass("shrink");
+      document.getElementById("logo").setAttribute("style", "max-height: 100%;");
     }
     else
     {
         $("header").removeClass("shrink");
+        document.getElementById("logo").setAttribute("style", "max-height: 75px;");
     }
 });
