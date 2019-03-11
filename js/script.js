ead6ada6f702c8b9cb1e8e9664b1bafed7f169a2
Jaren <jarenrcampbell@gmail.com>
2019-03-10 21:40:18 -0600

    Updated header tab colors (made them black). Trying to get hover effects working.

--- js/script.js
+++ js/script.js
@@ -4,29 +4,42 @@ $(document).on("scroll", function(){
       $("header").addClass("shrink");
       document.getElementById("logo").setAttribute("style", "max-height: 30px;");
 
-      var tabs = document.getElementsByClassName("tab");
-      for (var i = 0; i < tabs.length; i++) {
-          tabs[i].setAttribute("style", "background: #1d1e22; color: white; box-shadow: 0px 0px 2px 2px black;");
-      }
+    //   var tabs = document.getElementsByClassName("tab");
+    //   for (var i = 0; i < tabs.length; i++) {
+    //       tabs[i].setAttribute("style", "background: #1d1e22; color: white; box-shadow: 0px 0px 2px 2px black;");
+    //   }
 
-      var activeTab = document.getElementsByClassName("active");
-      for (var i = 0; i < activeTab.length; i++) {
-          activeTab[i].setAttribute("style", "background: red; color: black; box-shadow: 0px 0px 2px 2px black;");
-      }
+    $(".tab").css({"background": "black", "color": "white"});
+
+    //   var activeTab = document.getElementsByClassName("active");
+    //   for (var i = 0; i < activeTab.length; i++) {
+    //       activeTab[i].setAttribute("style", "background: #E00000; color: black; box-shadow: 0px 0px 2px 2px black;");
+    //   }
+
+    $(".active").css({"background": "#E00000", "color": "black"});
+
     }
     else
     {
         $("header").removeClass("shrink");
         document.getElementById("logo").setAttribute("style", "max-height: 75px;");
 
-        var tabs = document.getElementsByClassName("tab");
-        for (var i = 0; i < tabs.length; i++) {
-            tabs[i].setAttribute("style", "background: transparent; color: white; box-shadow: ;");
-        }
+        // var tabs = document.getElementsByClassName("tab");
+        // for (var i = 0; i < tabs.length; i++) {
+        //     tabs[i].setAttribute("style", "background: transparent; color: white; box-shadow: ;");
+        // }
 
-        var activeTab = document.getElementsByClassName("active");
-        for (var i = 0; i < activeTab.length; i++) {
-            activeTab[i].setAttribute("style", "background: transparent; color: red; box-shadow: ;");
-        }
+        $(".tab").css({"background": "transparent", "color": "white"});
+
+        // var activeTab = document.getElementsByClassName("active");
+        // for (var i = 0; i < activeTab.length; i++) {
+        //     activeTab[i].setAttribute("style", "background: transparent; color: #E00000; box-shadow: ;");
+        // }
+
+        $(".active").css({"background": "transparent", "color": "#E00000"});
     }
+});
+
+$(".tab").on("mouseover", function () {
+    $(".tab").css("background", "red");
 });
