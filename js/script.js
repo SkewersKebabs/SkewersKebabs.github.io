792f0d72e624483ec4c694ffea350d8e4b4e4027
Jaren <jarenrcampbell@gmail.com>
2019-03-11 13:36:00 -0600

    Renamed files, added a new font for navigation, logo does not expand until the very top of page.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 $(document).on("scroll", function(){
     if
-  ($(document).scrollTop() > ($(window).height() * .6)){
+  ($(document).scrollTop() > ($(window).height() * .5)){
         // $("header").css("background", "rgba(29,30,34, 1)");
         $("header").css("background", "black");
 
-        document.getElementById("logo").setAttribute("style", "max-height: 30px;");
+        // document.getElementById("logo").setAttribute("style", "max-height: 30px;");
 
         $(".tab").css({"background": "rgba(0, 0, 0, 1)", "color": "white"});
         $(".active").css({"background": "black", "color": "#E00000"});
 
-        $(".container-menu").css({"background": "#2f3138"});
+        // $(".container-menu").css({"background": "#2f3138"});
 
         $("header").css("opacity", "0.5");
     }
     else
     {
         $("header").css("background", "transparent");
-        document.getElementById("logo").setAttribute("style", "max-height: 75px;");
+        // document.getElementById("logo").setAttribute("style", "max-height: 75px;");
 
         $(".tab").css({"background": "transparent", "color": "white"});
         $(".active").css({"background": "transparent", "color": "#E00000"});
@@ -25,11 +25,18 @@ $(document).on("scroll", function(){
         
         $("header").css("opacity", 1);
     }
+
+    if ($(document).scrollTop() > 0) {
+        document.getElementById("logo").setAttribute("style", "max-height: 30px;")
+    }
+    else {
+        document.getElementById("logo").setAttribute("style", "max-height: 75px;")
+    }
 });
 
 
 
-$(document).on("scroll", function() { 
+$(document).ready( function() { 
         $("header").hover(function() { 
             if ($(document).scrollTop() > ($(window).height() * .6)){
                 $("header").css("opacity", "1");
