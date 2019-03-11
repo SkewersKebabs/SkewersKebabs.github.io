721cb3356cccbfe1c17e3d578d7513e3cdc36f53
Jaren <jarenrcampbell@gmail.com>
2019-03-10 19:08:00 -0600

    Gave tabs same color as header, increased header size, added box shadow.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ $(document).on("scroll", function(){
     if
   ($(document).scrollTop() > 20){
       $("header").addClass("shrink");
-      document.getElementById("logo").setAttribute("style", "max-height: 50px;");
+      document.getElementById("logo").setAttribute("style", "max-height: 30px;");
 
       var tabs = document.getElementsByClassName("tab");
       for (var i = 0; i < tabs.length; i++) {
-          tabs[i].setAttribute("style", "background: #393f4d; color: white;");
+          tabs[i].setAttribute("style", "background: #1d1e22; color: white; box-shadow: 0px 0px 2px 2px black;");
       }
 
       var activeTab = document.getElementsByClassName("active");
       for (var i = 0; i < activeTab.length; i++) {
-          activeTab[i].setAttribute("style", "background: red; color: black;");
+          activeTab[i].setAttribute("style", "background: red; color: black; box-shadow: 0px 0px 2px 2px black;");
       }
     }
     else
@@ -21,12 +21,12 @@ $(document).on("scroll", function(){
 
         var tabs = document.getElementsByClassName("tab");
         for (var i = 0; i < tabs.length; i++) {
-            tabs[i].setAttribute("style", "background: transparent; color: white;");
+            tabs[i].setAttribute("style", "background: transparent; color: white; box-shadow: ;");
         }
 
         var activeTab = document.getElementsByClassName("active");
         for (var i = 0; i < activeTab.length; i++) {
-            activeTab[i].setAttribute("style", "background: transparent; color: red;");
+            activeTab[i].setAttribute("style", "background: transparent; color: red; box-shadow: ;");
         }
     }
 });
